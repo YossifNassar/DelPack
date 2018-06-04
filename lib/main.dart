e22466fc988ae96dbc14bc11d64242b5660f5511
Yossif Nassar <ynassar@outbrain.com>
2018-06-04 10:27:02 +0300

    using image picker instead

--- lib/main.dart
+++ lib/main.dart
@@ -2,49 +2,44 @@ import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:googleapis/vision/v1.dart' as vision;
-import 'package:google_sign_in/google_sign_in.dart'
-    show GoogleSignIn;
+import 'package:google_sign_in/google_sign_in.dart' show GoogleSignIn;
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/services.dart' show rootBundle,ByteData;
-import 'imagescreen.dart';
 import "package:googleapis_auth/auth_io.dart";
+import 'package:image_picker/image_picker.dart';
 
 List<CameraDescription> cameras;
 vision.VisionApi visionApi;
 AuthClient client;
 
-Future<ByteData> _getImageBytes(String image) async{
-  return await rootBundle.load(image);
-}
-
-Future<Null> _annotateImage(String filePath) async {
-  var bytes = await _getImageBytes(filePath);
+Future<Null> _annotateImage(List<int> bytes) async {
   var imp = visionApi.images;
   var request = vision.AnnotateImageRequest()
     ..features = [vision.Feature()..type = "DOCUMENT_TEXT_DETECTION"];
+//  var imageSource = vision.ImageSource()
+//    ..imageUri = "https://cloud.google.com/vision/docs/images/abbey_road.JPG";
   var image = vision.Image()
-    ..contentAsBytes = bytes.buffer.asUint8List().cast();
+//    ..source = imageSource;
+    ..contentAsBytes = bytes;
   request.image = image;
   var annotateRequest = vision.BatchAnnotateImagesRequest()
     ..requests = [request];
+  print("Sending request at: ${DateTime.now()}");
   var res = await imp.annotate(annotateRequest);
+  print("Receiving response at: ${DateTime.now()}");
   res?.responses?.forEach((r) {
     r.textAnnotations.forEach((txt) {
       print(txt.description);
     });
   });
-
+  print("Done printing annotations at: ${DateTime.now()}");
 }
 
 Future<Null> main() async {
   cameras = await availableCameras();
 
   final _googleSignIn = new GoogleSignIn(
-    scopes: [
-      'email'
-    ],
+    scopes: ['email'],
   );
   final FirebaseAuth _auth = FirebaseAuth.instance;
   var googleUser = await _googleSignIn.signIn();
@@ -55,21 +50,25 @@ Future<Null> main() async {
   );
   print("signed in ${user.displayName}");
 
-
   final accountCredentials = new ServiceAccountCredentials.fromJson({
-    //JSON
+    "type": "service_account",
+    "project_id": "delpack-206016",
+    "private_key_id": "7b197b039db3b5000ca1553a0997d2a1b9896518",
+    "private_key":
+        "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCDoaK3RAMBhhVD\ndV7kJYRRzsZOw3sDdQmF3jWtrffIOxQM5begJDCUFZBn6IOQq4rKm70U0WxTWjg/\n0XxiGTzsMQ4Emk5SrBXVowNEOJ/t5P5f04RwsVa9iMJpz1yR9r9rCrRVJb/+l52e\nZpAf0cTBJv4G5TQRICb28SMk3COrU/JSS0M46Sx8IGHX2NypLibeMJjfKKt8HAfk\nkdngz2Z3MvHuNuRAtKhttApQvivn3gMqzNLTUUmnDZO7pplgOIExKkEzCadoU5L2\nbdCNU0uAPgQTMACCkEA6XzTxwXUbPvx5l4GcSjfio+LfsqPs99B3URiHgoo8lPW/\nliSXymNlAgMBAAECggEAAzUs5WauXBJYrYHDyBWnTKCpWDj1bJl6E0P/zHnXfyhR\nZ7NSd5EdZ4s5prlAW4Xn+67uTGToxHwHsDeC76iFyjZeN8AYwqhTtCe+rF3cWxCP\nHyptdfCCy9KN4+a+RBfNYuaqR2n70vtkvtnX7QPqCOFapDQb0xzheFXBE6BXlrcU\nTvRPLsfLuonI4llHQviK5h6Le7yNylILJunNHKlMhxd0AhtoJwSN7+Kkitb8prcb\ncohP3Xsx5hDvOBSaCyobCM+RZz48WktfZ70Vv0iKRm76X7xmR88Wdgp57tDcc6y7\nXkythntCwRGHWXgJMwwM56FLMoBwyknt2UkWFZYwkQKBgQC5Nu2OsN7pS1HO1rKy\nbfmHjnUn0D9k4zeu/OE1nr17pVRTUMNT6Plb4wCZ8ZmDBid4scejNZYT08EFEm4n\niTHqh3sbqCmPDSorBB+CBPdxAxX3c/Usx1m+13lduEHtBap0ZQJ+11cxHZKD/UnH\niyWMKFGQ6ek53ogmo4cW8FlGwwKBgQC18DkHhGZGaDfntCkc2tjONBnoYO7YJ01k\na8pTzUnvtzMkf7WKvTkohdIPs4MJbSSwrjDfLof5DbRvdybrs2UsOvweFOoItkCv\npObL7icc3Y8xHmEPU8Orm8T5V3wZ4x9OnYed8LjznCiDooTQ3BXlrfeYnkFfZ1E4\nxeJxa4MatwKBgQCdWhsMxRs8XG8OKLz8dg4uqkrCHlwdXCSsl87FfS1B5L31YoHv\nwdhHxl12bBmdhRb3pSIUeKweACLWKkhHcnlj5OIkJwPXsrmGfiQ+k/Wcn6CD8JH1\nDxAh4HINu0LXGZ8s77vlAxHieltFo2SREJg/sOlLEk7AiVxmZzuXs25FfQKBgQCo\nxXCBsqYVRfQmMg+5PqHDlr05vUGSrdKzBvKnDlY50p3Ui8tiKent10VnX0R86AwT\nAWNAhS+JLtdkVwl5t+5wOz9IwGnhLv+Ob8/Q+Ni7hCOikuXCjBeFB3iPPEJY2UFf\nce4t6vIGuaMziPzfqml9qAV4Q8WstHC504cdY3g4vQKBgEdTvIblISC7jO/07ZT2\nWgRa3b+nlP//XJcqzqYM3TK7cqMB4EShlPHZDfkU3LzUMg/mUEtv/YYR350vQ+mr\noHIqL7YdZ6e0NIx0SbQavylVIsK23G9iLR2x/K7ZrEhthOfz7JDSSPw748Wt66y5\nTI00Ad0Ywjw0RkYUIa5R6cqN\n-----END PRIVATE KEY-----\n",
+    "client_email":
+        "delpack-service-account@delpack-206016.iam.gserviceaccount.com",
+    "client_id": "114220544307704907188",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://accounts.google.com/o/oauth2/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url":
+        "https://www.googleapis.com/robot/v1/metadata/x509/delpack-service-account%40delpack-206016.iam.gserviceaccount.com"
   });
-  var scopes =  [
-      'https://www.googleapis.com/auth/cloud-vision'
-    ];
+  var scopes = ['https://www.googleapis.com/auth/cloud-vision'];
 
-  var prompt = (String url) {
-    print("Please go to the following URL and grant access:");
-    print("  => $url");
-    print("");
-  };
-
-  client = await clientViaServiceAccount(accountCredentials, scopes).then((AuthClient client) {
+  client = await clientViaServiceAccount(accountCredentials, scopes)
+      .then((AuthClient client) {
     // [client] is an authenticated HTTP client.
     return client;
   });
@@ -78,128 +77,63 @@ Future<Null> main() async {
   visionApi = vision.VisionApi(client);
 
   runApp(new MaterialApp(
-    title: 'FoodyV',
-    home: new FirstScreen("User: none"),
+    title: 'DelPack',
+    home: new FirstScreen("User: ${user.displayName}"),
   ));
 }
 
 class FirstScreen extends StatelessWidget {
   String _loggedUser;
+
   FirstScreen(String loggedUser) {
     this._loggedUser = loggedUser;
   }
 
   @override
   Widget build(BuildContext context) {
-    return new CameraApp(_loggedUser);
+    return new CameraApp();
   }
 }
 
 class CameraApp extends StatefulWidget {
-  String loggedUser;
-  CameraApp(String loggedUser) {
-    this.loggedUser = loggedUser;
-  }
-
   @override
-  _CameraAppState createState() => new _CameraAppState(loggedUser);
+  _CameraApp createState() => new _CameraApp();
 }
 
-class _CameraAppState extends State<CameraApp> {
-  CameraController controller;
-  String _filePath;
-  String loggedUser;
+class _CameraApp extends State<CameraApp> {
+  File _image;
 
-  _CameraAppState(String loggedUser) {
-    this.loggedUser = loggedUser;
-  }
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    var bytes = image.readAsBytesSync();
+    _annotateImage(bytes);
 
-  void _showCameraException(CameraException e) {
-    print("${e.code}, ${e.description}");
-  }
-
-  String timestamp() => new DateTime.now().millisecondsSinceEpoch.toString();
-
-  void takePicture() async {
-    if (!controller.value.isInitialized) {
-      print('Error: select a camera first.');
-      return;
-    }
-    final Directory extDir = await getApplicationDocumentsDirectory();
-    final String dirPath = '${extDir.path}/Pictures/flutter_test';
-    await new Directory(dirPath).create(recursive: true);
-    final String filePath = '$dirPath/${timestamp()}.jpg';
-
-    if (controller.value.isTakingPicture) {
-      // A capture is already pending, do nothing.
-      return;
-    }
-    File file = new File(filePath);
-    if(file.existsSync()) {
-      file.deleteSync();
-    }
-    try {
-      await controller.takePicture(filePath);
-    } on CameraException catch (e) {
-      _showCameraException(e);
-      return;
-    }
-
-    _filePath = filePath;
-    _navigateToImageScreen();
-    _annotateImage(_filePath);
-  }
-
-  Widget _cameraFloatingWidget() {
-    return new Align(
-        alignment: Alignment.bottomRight,
-        child: new FloatingActionButton(
-          onPressed: takePicture,
-          tooltip: "take a shot",
-          child: new Icon(Icons.photo_camera, size: 30.0),
-        ));
-  }
-
-  void _navigateToImageScreen(){
-    Navigator.push(
-      context,
-      new MaterialPageRoute(builder: (context) => new ImageScreen(filePath: _filePath)),
-    );
+    setState(() {
+      _image = image;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    controller = new CameraController(cameras[0], ResolutionPreset.low);
-    controller.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    controller?.dispose();
-    client.close();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!controller.value.isInitialized) {
-      return new Container();
-    }
-    return
-      new Scaffold(
-        appBar: new AppBar(
-          title: new Text("$loggedUser"),
-        ),
-        body: new Center(
-          child: new CameraPreview(controller),
-        ),
-        floatingActionButton: _cameraFloatingWidget(),
-      );
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Image Picker Example'),
+      ),
+      body: new Center(
+        child: _image == null
+            ? new Text('No image selected.')
+            : new Image.file(_image),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: new Icon(Icons.add_a_photo),
+      ),
+    );
   }
 }
